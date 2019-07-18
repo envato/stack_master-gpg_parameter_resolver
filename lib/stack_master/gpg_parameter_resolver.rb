95729883601ef9e7396f5e2f3916885e7b5e06c1
Orien Madgwick <_@orien.io>
2019-07-19 00:48:57 +1000

    Remove class stub

--- lib/stack_master/gpg_parameter_resolver.rb
+++ lib/stack_master/gpg_parameter_resolver.rb
@@ -1,10 +1,3 @@
 require "stack_master"
 require "stack_master/gpg_parameter_resolver/version"
 require "stack_master/parameter_resolvers/secret"
-
-module StackMaster
-  module GpgParameterResolver
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
